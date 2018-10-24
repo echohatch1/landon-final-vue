d7ac3d9ee7f141ca9b56bf4c2a3cd3467846c19f
Landon Hatch <landonhatch@gmail.com>
2018-10-24 14:45:03 -0600

    Latest changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,42 @@
 <template>
-  <div class="about">
+<v-container grid-list-xl>
+  <v-layout row wrap>
+    <v-flex v-for="ship in starships" :key="ship.model" xs6>
+    <v-card>
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">h3 is here</h3>
+          <div>sub div is here</div>
+          <span>{{ ship.manufacturer }}</span>
+        </div>
+      </v-card-title>
+      <v-btn flat color="blue">Search</v-btn>
+    </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
+  <!--<div class="about">
     <h1>This is an about page</h1>
     <ul id="example-1">
   <li v-for="film in allFilms" :key="film.episode_id">
     {{ film.title }}
   </li>
 </ul>
+  <div>
+    {{ starships[0] }}
   </div>
+  </div>-->
 </template>
 
 <script>
 import { films } from "../assets/films.js";
+import { starships } from "../assets/starships.js";
+import { planets } from "../assets/planets.js";
 export default {
   data: function() {
     return {
-    allFilms: films
+    allFilms: films,
+    starships
     }
   }
 }
---
+++ src/views/Layout.vue
@@ -0,0 +1,29 @@
+<template>
+<v-app>
+    <v-navigation-drawer app v-model="drawer" clipped>Nav</v-navigation-drawer>
+        <v-toolbar app clipped-left>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        </v-toolbar>
+            <v-content>
+                <v-container fluid>
+                    <div>Content</div>
+                </v-container>
+            </v-content>
+        <v-footer app>Footer</v-footer>
+</v-app>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            drawer: null
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
