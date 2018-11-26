b3f8db2cc372abd100b70d4c667453a3fc271765
Landon Hatch <landonhatch@gmail.com>
2018-11-26 15:29:34 -0700

    Fixed styling on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,10 @@
     <h2>Seach Images</h2>
     <form @submit.prevent="getResult(query)">
       <v-layout row wrap>
-      <v-flex xs6>
+      <v-flex xs12 md8>
         <v-text-field label="Type in your search" v-model="query"></v-text-field>
       </v-flex>
-      <v-btn large outline flat @click="getResult(query)" class="green darken-3">Search</v-btn>
+      <v-flex xs12 md2>
       <v-select
       v-model="select"
       :items="items"
@@ -14,41 +14,43 @@
       label="Number of Results"
       @change="numResults = select"
     ></v-select>
+    </v-flex>
+    <v-btn large outline flat @click="getResult(query)" class="green darken-3">Search</v-btn>
       </v-layout>
     </form>
     <div class="results" v-if="results">
 
       <v-layout row wrap>
 
-      <v-flex fluid xs12 md6 lg4 v-for="result in results">
-        <v-card>
+      <v-flex d-flex xs12 md6 lg4 v-for="result in results">
+        <v-card class="card">
 
           <figure>
-          <figcaption><h3>{{ result.description.toUpperCase() }}</h3></figcaption>
           <img class="main-images":src="result.urls.small"/>
+              <figcaption><h3>{{ result.description }}</h3></figcaption>
           </figure>
 
-          <v-layout class="card-links" align-center justify-space-between row fill-height>
-            <v-container>
+          <v-layout class="card-links" align-center>
+   
 
+            <v-layout justify-start>
             <v-btn class="avatar" large flat round color="black" :href="result.user.portfolio_url">
-              
                 <v-list-tile-avatar>
                   <img class="profile-images":src="result.user.profile_image.small"/>
                 </v-list-tile-avatar>
               
           <v-link flat color="black">{{ result.user.username }}</v-link>
             </v-btn>
+            </v-layout>
 
-            </v-container>
 
-            <v-container>
+       
               <v-layout justify-end>
                 <v-btn class="download-button" flat round fab color="black" :href="result.links.download">
                   <v-icon>cloud_download</v-icon>
                 </v-btn>
               </v-layout>
-            </v-container>
+
           
           </v-layout>
 
@@ -96,22 +98,28 @@ export default {
 //"https://api.unsplash.com/photos/search/?query=" + query +  "&client_id=" + CLIENT_ID
 
 <style scoped>
-.results figure {
+/*.results figure {
   padding: 20px;
   padding-top: 10px;
   padding-bottom: 0;
+}*/
+.card {
+  padding: 20px;
 }
 .results figure figcaption {
-
+  font-size: 10px;
 }
 .results .main-images {
     width: 100%;
 }
 
+.profile-images {
+  margin: 0;
+}
 
 .avatar {
   padding-left: 0;
-  margin:0;
+  margin-left: 0;
 }
 
 .download-button {
